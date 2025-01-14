68331863ca9781db466668b0bb909860125595ec
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-01-14 18:04:39 +0100

    implement integration over z_list

--- src/numerov/rydberg.py
+++ src/numerov/rydberg.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Optional, TypeVar, Union
 
 import numpy as np
@@ -29,13 +30,14 @@ class RydbergState:
         run_backward (default: True): Wheter to integrate the radial Schrödinger equation "backward" of "forward".
         epsilon_u (default: 1e-10): The initial magnitude of the radial wavefunction at the outer boundary.
             For forward integration we set u[0] = 0 and u[1] = epsilon_u,
-            for backward integration we set u[-1] = 0 and u[-2] = (-1)^{(n - l + 1) % 2} * epsilon_u.
-        xmin (default see `RydbergState._set_x_range`): The minimum value of the radial coordinate
+            for backward integration we set u[-1] = 0 and u[-2] = (-1)^{(n - l - 1) % 2} * epsilon_u.
+        xmin (default see `RydbergState.set_range`): The minimum value of the radial coordinate
         in dimensionless units.
-        xmax (default see `RydbergState._set_x_range`): The maximum value of the radial coordinate
+        xmax (default see `RydbergState.set_range`): The maximum value of the radial coordinate
         in dimensionless units.
-        dx (default see `RydbergState._set_x_range`): The step size of the integration in dimensionless units
+        dx (default see `RydbergState.set_range`): The step size of the integration in dimensionless units
         (corresponds to h in the equation above).
+        steps (default see `RydbergState.set_range`): The number of steps of the integration (use either steps or dx).
         parameter_dict (default: None): A dictionary containing the parameters for the effective potential.
         If not provided, the parameters are loaded from the database.
     """
@@ -52,6 +54,7 @@ class RydbergState:
     xmin: Optional[float] = None
     xmax: Optional[float] = None
     dx: Optional[float] = None
+    steps: Optional[int] = None
 
     parameter_dict: Optional[dict[str, float]] = None
 
@@ -73,7 +76,7 @@ class RydbergState:
             self._load_hydrogen_like_parameters()
         else:
             self._load_parameters_from_database()
-        self._set_x_range()
+        self.set_range()
 
     def _load_parameters_from_dict(self) -> None:
         assert self.parameter_dict is not None
@@ -97,62 +100,97 @@ class RydbergState:
         self.Z = {"H": 1, "He+": 2}[self.species]
         self.energy = -(self.Z**2) / (self.n**2)
         self.a1 = self.a2 = self.a3 = self.a4 = 0
-        self.alphac = 0
+        self.ac = 0
         self.xc = np.inf
 
-    def _set_x_range(self) -> None:
-        """Automatically determine sensful default values for xmin, xmax and dx."""
-        self.dx = 5e-5
-        self.xmin = self.dx
-        self.xmax = 80
+        self.alpha1, self.alpha2, self.alpha3, self.alpha4 = self.a1, self.a2, self.a3, self.a4
+        self.zc = self.xc
+        self.alphac = self.ac
 
-    def calc_Veff(self, x: ValueType) -> ValueType:
-        # TODO check units
-        # TODO add pint quantities, e.g. for alpha, ...
-        Z_nl = 1 + (self.Z - 1) * np.exp(-self.a1 * x) - x * (self.a3 + self.a4 * x) * np.exp(-self.a2 * x)
-        V_c = -2 * Z_nl / x  # TODO check 2 in diemnsionless units
-        V_p = -self.alphac / (2 * x**4) * (1 - np.exp(-((x / self.xc) ** 6)))
-        if self.species in ["H", "He+"]:
-            V_SO = 0
+    def set_range(self) -> None:
+        """Automatically determine sensful default values for xmin, xmax and dx.
+
+        The x-values represent the raidal coordinate in units of the Bohr radius a_0 (x = r / a_0).
+        Furthermore, we define z-values as z = sqrt(x) = sqrt(r / a_0), which is used for the integration.
+        The benefit of using z is that the nodes of the wavefunction are equally spaced in z-space,
+        allowing for a computational better choice of choosing the constant step size during the integration.
+
+
+        """
+        if self.xmin is None:
+            self.xmin = 5e-5
+        if self.xmax is None:
+            self.xmax = 80
+        zmin, zmax = np.sqrt(self.xmin), np.sqrt(self.xmax)
+
+        if self.dx is not None and self.steps is not None:
+            raise ValueError("Use either dx or steps, not both.")
+        elif self.dx is None:
+            if self.steps is None:
+                self.steps = 1000
+            self.z_list = np.linspace(zmin, zmax, self.steps, endpoint=True)
+        elif self.dx is not None:
+            dz = np.sqrt(self.dx)
+            self.z_list = np.arange(zmin, zmax + dz, dz)
+
+        self.dz = self.z_list[1] - self.z_list[0]
+        self.steps = len(self.z_list)
+
+    @cached_property
+    def V_tot(self) -> np.ndarray:
+        z = self.z_list
+        z2 = z**2
+        z4 = z**4
+        z6 = z**6
+        z8 = z**8
+
+        Z_nl = 1 + (self.Z - 1) * np.exp(-self.a1 * z2) - z2 * (self.a3 + self.a4 * z2) * np.exp(-self.a2 * z2)
+        V_c = -Z_nl / z2 * 2  # TODO check 2 in diemnsionless units
+        V_p = -self.alphac / (2 * z8) * (1 - np.exp(-((z / self.zc) ** 12)))
+        if self.species in ["H", "He+"]:  # TODO or self.l > 4 ???
+            V_so = 0
         else:
             alpha = 1 / 137.035999084
-            V_SO = (
-                alpha / (4 * x**3) * (self.j * (self.j + 1) - self.l * (self.l + 1) - self.s * (self.s + 1))
+            V_so = (
+                alpha / (4 * z6) * (self.j * (self.j + 1) - self.l * (self.l + 1) - self.s * (self.s + 1))
             )  # TODO check wheter 1 or 1/4?
-        V_l = self.l * (self.l + 1) / (x**2)
-        Veff = V_c + V_p + V_SO + V_l
-        return Veff
+        V_l = self.l * (self.l + 1) / z4
+        V_sqrt = (3 / 16) / z4
+        return V_c + V_p + V_so + V_l + V_sqrt
 
-    def calc_g_list(self, x: ValueType) -> ValueType:
-        return self.energy - self.calc_Veff(x)
+    @cached_property
+    def g_list(self) -> np.ndarray:
+        return 4 * self.z_list**2 * (self.energy - self.V_tot)
 
     def integrate(self) -> tuple[np.ndarray, np.ndarray]:
         r"""Run the Numerov integration of the radial Schrödinger equation for the desired state,
 
         Returns:
-            x_list: A numpy array of the values of the radial coordinate at which the wavefunction was evaluated.
-            u_list: A numpy array of the values of the radial wavefunction at each value of x_list.
+            z_list: A numpy array of the z-values at which the wavefunction was evaluated (z = sqrt(r/a_0))
+            w_list: A numpy array of the function w(z) = z^{-1/2} \tilde{u}(x=z^2) = (r/a_0)^{-1/4} sqrt(a_0) r R(r)
+            (where we used \tilde{u}(x) = sqrt(a_0) r R(r)
                 The radial wavefunction is normalized such that
 
                 .. math::
-                    \int_{0}^{\infty} x^2 |R(x)|^2 dx = \int_{0}^{\infty} |u(x)|^2 dx = 1
+                    \int_{0}^{\infty} r^2 |R(x)|^2 dr
+                    = \int_{0}^{\infty} |\tilde{u}(x)|^2 dx = 1
+                    = \int_{0}^{\infty} 2 z^2 |w(z)|^2 dz = 1
         """
-        self.x_list = x_list = np.arange(self.xmin, self.xmax + self.dx, self.dx)
-        self.g_list = g_list = self.calc_g_list(x_list)
 
         if self.run_backward:
-            y0, y1 = 0, (-1) ** ((self.n - self.l + 1) % 2) * self.epsilon_u
+            # Note: n - l - 1 is the number of nodes of the radial wavefunction
+            # Thus, the sign of the wavefunction at the outer boundary is (-1)^{(n - l - 1) % 2}
+            y0, y1 = 0, (-1) ** ((self.n - self.l - 1) % 2) * self.epsilon_u
         else:  # forward
             y0, y1 = 0, self.epsilon_u
 
         if self._use_njit:
-            self.u_list = run_numerov_integration(self.dx, len(x_list), y0, y1, g_list, self.run_backward)
+            self.w_list = run_numerov_integration(self.dz, self.steps, y0, y1, self.g_list, self.run_backward)
         else:
-            self.u_list = _python_run_numerov_integration(self.dx, len(x_list), y0, y1, g_list, self.run_backward)
+            self.w_list = _python_run_numerov_integration(self.dz, self.steps, y0, y1, self.g_list, self.run_backward)
 
-        # normalize the wavefunction, such that
-        # \int_{0}^{\infty} x^2 |R(x)|^2 dx = \int_{0}^{\infty} |u(x)|^2 dx = 1
-        norm = np.sqrt(np.sum(self.u_list**2) * self.dx)
-        self.u_list /= norm
+        # normalize the wavefunction, see docstring
+        norm = np.sqrt(2 * np.sum(self.w_list**2 * self.z_list**2) * self.dz)
+        self.w_list /= norm
 
-        return self.x_list, self.u_list
+        return self.z_list, self.w_list
