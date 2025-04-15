dfad7d2bddba2988bc782dccdf8d90b47323eaba
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-04-15 18:11:47 +0200

    improve readme and docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.extlinks",
     "nbsphinx",
-    "sphinx.ext.inheritance_diagram",
     "sphinx_autodoc_typehints",
+    "myst_parser",
 ]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "_doctrees", "Thumbs.db", ".DS_Store"]  # Ignore these source files and folders
