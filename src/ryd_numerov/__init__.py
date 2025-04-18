e6bbdbc92a977d45c53cd0626c6ff6593b96f48b
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-04-18 19:49:02 +0200

    start writing elements as python files

--- src/ryd_numerov/__init__.py
+++ src/ryd_numerov/__init__.py
@@ -1,10 +1,11 @@
-from ryd_numerov import angular, model, radial
+from ryd_numerov import angular, elements, model, radial
 from ryd_numerov.rydberg import RydbergState
 from ryd_numerov.units import ureg
 
 __all__ = [
     "RydbergState",
     "angular",
+    "elements",
     "model",
     "radial",
     "ureg",
