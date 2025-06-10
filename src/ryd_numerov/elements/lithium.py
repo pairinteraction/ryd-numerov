80dd9838178bb0b448209592687d1298e818b373
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-06-10 10:32:17 +0200

    move parameters quantum defects to elements

--- src/ryd_numerov/elements/lithium.py
+++ src/ryd_numerov/elements/lithium.py
@@ -1,3 +1,5 @@
+from typing import ClassVar
+
 from ryd_numerov.elements.element import Element
 
 
@@ -8,3 +10,18 @@ class Lithium(Element):
 
     # https://webbook.nist.gov/cgi/inchi?ID=C7439932&Mask=20
     _ionization_energy = (5.391_72, None, "eV")
+
+    # -- [1] Phys. Rev. A 34, 2889 (1986) (Li 7)
+    # -- [2] T. F. Gallagher, ``Rydberg Atoms'', Cambridge University Press (2005), ISBN: 978-0-52-102166-1
+    # -- [3] Johansson I 1958 Ark. Fysik 15 169
+    _quantum_defects: ClassVar = {
+        (0, 0.5): (0.3995101, 0.029, 0, 0, 0),  # [1]
+        (1, 0.5): (0.0471780, -0.024, 0, 0, 0),  # [1]
+        (1, 1.5): (0.0471665, -0.024, 0, 0, 0),  # [1]
+        (2, 1.5): (0.002129, -0.01491, 0.1759, -0.8507, 0),  # [2,3]
+        (2, 2.5): (0.002129, -0.01491, 0.1759, -0.8507, 0),  # [2,3]
+        (3, 2.5): (0.000305, -0.00126, 0, 0, 0),  # [2,3]
+        (3, 3.5): (0.000305, -0.00126, 0, 0, 0),  # [2,3]
+    }
+
+    _corrected_rydberg_constant = (109728.64, None, "1/cm")
--- src/ryd_numerov/elements/rubidium.py
+++ src/ryd_numerov/elements/rubidium.py
@@ -1,8 +1,9 @@
+from typing import ClassVar
+
 from ryd_numerov.elements.element import Element
 
 
-class Rubidium(Element):
-    species = "Rb"
+class _RubidiumAbstract(Element):
     s = 1 / 2
     ground_state_shell = (5, 0)
 
@@ -13,3 +14,36 @@ class Rubidium(Element):
     # https://webbook.nist.gov/cgi/inchi?ID=C7440177&Mask=20
     # _ionization_energy = (4.177_13, 0.000_002, "eV")  # noqa: ERA001
     # corresponds to (1_010_025.54, 0.48, "GHz")
+
+    # -- [1] Phys. Rev. A 83, 052515 (2011) - Rb87
+    # -- [2] Phys. Rev. A 67, 052502 (2003) - Rb85
+    # -- [3] Phys. Rev. A 74, 054502 (2006) - Rb85
+    # -- [4] Phys. Rev. A 74, 062712 (2006) - Rb85
+    _quantum_defects: ClassVar = {
+        (0, 0.5): (3.1311807, 0.1787, 0, 0, 0),  # [1]
+        (1, 0.5): (2.6548849, 0.29, 0, 0, 0),  # [2]
+        (1, 1.5): (2.6416737, 0.295, 0, 0, 0),  # [2]
+        (2, 1.5): (1.3480948, -0.6054, 0, 0, 0),  # [1]
+        (2, 2.5): (1.3464622, -0.594, 0, 0, 0),  # [1]
+        (3, 2.5): (0.0165192, -0.085, 0, 0, 0),  # [3]
+        (3, 3.5): (0.0165437, -0.086, 0, 0, 0),  # [3]
+        (4, 3.5): (0.004, 0, 0, 0, 0),  # [4]
+        (4, 4.5): (0.004, 0, 0, 0, 0),  # [4]
+    }
+
+
+class Rubidium87(_RubidiumAbstract):
+    species = "Rb87"
+
+    _corrected_rydberg_constant = (109736.62301604665, None, "1/cm")
+
+
+class Rubidium(Rubidium87):
+    # for backwards compatibility
+    species = "Rb"
+
+
+class Rubidium85(_RubidiumAbstract):
+    species = "Rb85"
+
+    _corrected_rydberg_constant = (109736.605, None, "1/cm")
