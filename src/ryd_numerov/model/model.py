4cd860de5901867daf211cc1df09b685564bd12f
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-06-12 21:04:44 +0200

    improve turning point

--- src/ryd_numerov/model/model.py
+++ src/ryd_numerov/model/model.py
@@ -1,5 +1,5 @@
 import logging
-from typing import TYPE_CHECKING, Literal, Optional, get_args
+from typing import TYPE_CHECKING, Literal, Optional, TypeVar, get_args
 
 import numpy as np
 
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 
 PotentialType = Literal["coulomb", "model_potential_marinescu_1993"]
 
+XType = TypeVar("XType", "NDArray", float)
+
 
 class Model:
     """Model to describe the potentials for an atomic state."""
@@ -19,27 +21,19 @@ class Model:
     def __init__(
         self,
         element: "BaseElement",
-        n: int,
         l: int,
-        j: float,
         potential_type: Optional[PotentialType] = None,
     ) -> None:
         r"""Initialize the model.
 
         Args:
             element: BaseElement object representing the atomic species.
-            n: Principal quantum number
             l: Orbital angular momentum quantum number
-            j: Total angular momentum quantum number
             potential_type: Which potential to use for the model.
 
         """
         self.element = element
         self.l = l
-
-        # n and j are only used for the turning point calculation ...
-        self.n = n
-        self.j = j
 
         if potential_type is None:
             potential_type = self.element.potential_type_default
@@ -49,7 +43,7 @@ class Model:
             raise ValueError(f"Invalid potential type {potential_type}. Must be one of {get_args(PotentialType)}.")
         self.potential_type = potential_type
 
-    def calc_potential_coulomb(self, x: "NDArray") -> "NDArray":
+    def calc_potential_coulomb(self, x: "XType") -> "XType":
         r"""Calculate the Coulomb potential V_Col(x) in atomic units.
 
         The Coulomb potential is given as
@@ -68,7 +62,7 @@ class Model:
         """
         return -1 / x
 
-    def calc_model_potential_marinescu_1993(self, x: "NDArray") -> "NDArray":
+    def calc_model_potential_marinescu_1993(self, x: "XType") -> "XType":
         r"""Calculate the model potential by Marinescu et al. (1994) in atomic units.
 
         The model potential from
@@ -100,7 +94,7 @@ class Model:
         a1, a2, a3, a4 = parameter_dict.get(self.l, parameter_dict[max(parameter_dict.keys())])
         exp_a1 = np.exp(-a1 * x)
         exp_a2 = np.exp(-a2 * x)
-        z_nl: NDArray = 1 + (self.element.Z - 1) * exp_a1 - x * (a3 + a4 * x) * exp_a2
+        z_nl: XType = 1 + (self.element.Z - 1) * exp_a1 - x * (a3 + a4 * x) * exp_a2
         v_c = -z_nl / x
 
         alpha_c = self.element.alpha_c_marinescu_1993
@@ -112,15 +106,15 @@ class Model:
                 raise ValueError("No parametric model potential parameters defined for this element.")
             # default to x_c for the maximum l
             x_c = r_c_dict.get(self.l, r_c_dict[max(r_c_dict.keys())])
-            x2: NDArray = x * x
-            x4: NDArray = x2 * x2
-            x6: NDArray = x4 * x2
+            x2: XType = x * x
+            x4: XType = x2 * x2
+            x6: XType = x4 * x2
             exp_x6 = np.exp(-(x6 / x_c**6))
             v_p = -alpha_c / (2 * x4) * (1 - exp_x6)
 
         return v_c + v_p
 
-    def calc_effective_potential_centrifugal(self, x: "NDArray") -> "NDArray":
+    def calc_effective_potential_centrifugal(self, x: "XType") -> "XType":
         r"""Calculate the effective centrifugal potential V_l(x) in atomic units.
 
         The effective centrifugal potential is given as
@@ -140,7 +134,7 @@ class Model:
         x2 = x * x
         return (1 / self.element.reduced_mass_factor) * self.l * (self.l + 1) / (2 * x2)
 
-    def calc_effective_potential_sqrt(self, x: "NDArray") -> "NDArray":
+    def calc_effective_potential_sqrt(self, x: "XType") -> "XType":
         r"""Calculate the effective potential V_sqrt(x) from the sqrt transformation in atomic units.
 
         The sqrt transformation potential arises from the transformation from the wavefunction u(x) to w(z),
@@ -161,7 +155,7 @@ class Model:
         x2 = x * x
         return (1 / self.element.reduced_mass_factor) * (3 / 32) / x2
 
-    def calc_total_effective_potential(self, x: "NDArray") -> "NDArray":
+    def calc_total_effective_potential(self, x: "XType") -> "XType":
         r"""Calculate the total effective potential V_eff(x) in atomic units.
 
         The total effective potential includes all physical and effective potentials:
@@ -197,61 +191,76 @@ class Model:
             v = self.calc_potential_coulomb(x)
         elif self.potential_type == "model_potential_marinescu_1993":
             v = self.calc_model_potential_marinescu_1993(x)
+        else:
+            raise ValueError(f"Invalid potential type {self.potential_type}.")
 
         v += self.calc_effective_potential_centrifugal(x)
         v += self.calc_effective_potential_sqrt(x)
 
         return v
 
-    def calc_z_turning_point(self, which: Literal["hydrogen", "classical", "zerocrossing"], dz: float = 1e-3) -> float:
-        r"""Calculate the inner turning point z_i for the model.
+    def calc_hydrogen_turning_point_z(self, n: int, l: int) -> float:
+        r"""Calculate the classical turning point z_i of the state if it would be a hydrogen atom.
 
-        There are three different turning points we consider:
+        The hydrogen turning point is defined as the point,
+        where for the idealized hydrogen atom the potential equals the energy,
+        i.e. V_Col(r_i) + V_l(r_i) = E.
+        This is exactly the case at
 
-        - The hydrogen turning point, where for the idealized hydrogen atom the potential
-          equals the energy, i.e. V_Col(r_i) + V_l(r_i) = E.
-          This is exactly the case at
+        .. math::
+            r_i = n^2 - n \sqrt{n^2 - l(l + 1)}
 
-            .. math::
-                r_i = n^2 - n \sqrt{n^2 - l(l + 1)}
-
-        - The classical turning point, where the potential of the Rydberg model equals the energy,
-          i.e. V(r_i) + V_l(r_i) = E.
-
-        - The zero-crossing turning point, where the potential of the Rydberg model equals zero,
-          i.e. V(r_i) + V_l(r_i) = 0.
+        and z_i = sqrt{r_i / a_0}.
 
         Args:
-            which: Which turning point to calculate, one of "hydrogen", "classical", "zerocrossing".
+            n: Principal quantum number of the state.
+            l: Orbital angular momentum quantum number of the state.
+
+        Returns:
+            z_i: The inner hydrogen turning point z_i in the scaled dimensionless coordinate z_i = sqrt{r_i / a_0}.
+
+        """
+        return np.sqrt(n * n - n * np.sqrt(n * n - l * (l + 1)))  # type: ignore [no-any-return]
+
+    def calc_turning_point_z(self, n: int, l: int, j: float, dz: float = 1e-3) -> float:
+        r"""Calculate the classical inner turning point z_i for the given state.
+
+        The classical turning point is defined as the point,
+        where the potential of the Rydberg model equals the energy,
+        i.e. V(r_i) + V_l(r_i) = E.
+
+        Args:
+            n: Principal quantum number of the state.
+            l: Orbital angular momentum quantum number of the state.
+            j: Total angular momentum quantum number of the state.
             dz: The precision of the turning point calculation.
 
         Returns:
             z_i: The inner turning point z_i in the scaled dimensionless coordinate z_i = sqrt{r_i / a_0}.
 
         """
-        assert which in ["hydrogen", "classical", "zerocrossing"], f"Invalid turning point method {which}."
-        hydrogen_r_i: float = self.n * self.n - self.n * np.sqrt(self.n * self.n - self.l * (self.l - 1))
-        hydrogen_z_i: float = np.sqrt(hydrogen_r_i)
+        energy = self.element.calc_energy(n, l, j, unit="a.u.")
 
-        if which == "hydrogen":
-            return hydrogen_z_i
+        z_guess = self.calc_hydrogen_turning_point_z(n, l)
+        z_min, z_max = max(z_guess - 5, dz), z_guess + 5
 
-        if which == "classical":
-            z_list = np.arange(max(dz, hydrogen_z_i - 10), hydrogen_z_i + 10, dz)
-            energy = self.element.calc_energy(self.n, self.l, self.j, unit="a.u.")
-        elif which == "zerocrossing":
-            z_list = np.arange(max(dz, hydrogen_z_i / 2 - 5), hydrogen_z_i + 10, dz)
-            energy = 0
-
-        x_list = z_list * z_list
-        v_phys = self.calc_total_effective_potential(x_list)
-        arg: int = np.argwhere(v_phys < energy)[0][0]
-
-        if arg == 0:
-            if self.l == 0:
+        v_min = self.calc_total_effective_potential(z_min**2) - energy
+        if v_min < 0:
+            if l == 0 and z_min == dz:
                 return 0
-            logger.warning("Turning point is at arg=0, this shouldnt happen.")
-        elif arg == len(z_list) - 1:
-            logger.warning("Turning point is at maixmal arg, this shouldnt happen.")
+            raise ValueError(
+                f"Effective potential is below energy at z_min, this should not happen ({l=}, {z_min=:.2f})"
+            )
 
-        return z_list[arg]  # type: ignore [no-any-return]  # FIXME: numpy indexing
+        while z_max - z_min > dz:
+            z_list = np.linspace(z_min, z_max, 1_000, endpoint=True)
+            v_list = self.calc_total_effective_potential(z_list**2) - energy
+            inds = np.argwhere(v_list < 0)[:, 0]
+            if len(inds) == 0:
+                raise ValueError("Effective potential is always above energy, this should not happen!")
+            z_max = z_list[inds[0]]
+            if inds[0] > 0:
+                z_min = z_list[inds[0] - 1]
+                v_min = v_list[inds[0] - 1]
+
+        return z_min + (z_max - z_min) * v_min / (v_min - v_list[inds[0]])  # type: ignore [no-any-return]
