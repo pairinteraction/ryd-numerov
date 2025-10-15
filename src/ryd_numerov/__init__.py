9391bc2efc5967ef36b2d76d7cabc20e63913293
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-10-15 11:54:55 +0200

    rm rydberg_mqdt

--- src/ryd_numerov/__init__.py
+++ src/ryd_numerov/__init__.py
@@ -1,11 +1,9 @@
 from ryd_numerov import angular, elements, radial
 from ryd_numerov.rydberg import RydbergState, RydbergStateSQDT
-from ryd_numerov.rydberg_mqdt import RydbergStateMQDT
 from ryd_numerov.units import ureg
 
 __all__ = [
     "RydbergState",
-    "RydbergStateMQDT",
     "RydbergStateSQDT",
     "angular",
     "elements",
