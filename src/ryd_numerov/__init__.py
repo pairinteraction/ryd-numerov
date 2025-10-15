4201e21fc0b94a7bef49c4b5e08c5ab825ea11e3
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-10-15 12:29:38 +0200

    start updating RydbergState to use SpinState and RadialState

--- src/ryd_numerov/__init__.py
+++ src/ryd_numerov/__init__.py
@@ -1,10 +1,9 @@
 from ryd_numerov import angular, elements, radial
-from ryd_numerov.rydberg import RydbergState, RydbergStateSQDT
+from ryd_numerov.rydberg import RydbergStateAlkali
 from ryd_numerov.units import ureg
 
 __all__ = [
-    "RydbergState",
-    "RydbergStateSQDT",
+    "RydbergStateAlkali",
     "angular",
     "elements",
     "radial",
