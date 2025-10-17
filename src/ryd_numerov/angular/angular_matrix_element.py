06125961e1c4f19c5246a965a4953d150d581fc6
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-10-17 08:55:56 +0200

    add calc_prefactor_of_operator_in_coupled_scheme

--- src/ryd_numerov/angular/angular_matrix_element.py
+++ src/ryd_numerov/angular/angular_matrix_element.py
@@ -180,3 +180,39 @@ def spherical_like_matrix_element(l1: int, l2: int, operator: OperatorType, kapp
         prefactor *= np.sqrt((2 * kappa + 1) / (4 * np.pi))
     wigner_3j = calc_wigner_3j(l2, kappa, l1, 0, 0, 0)
     return prefactor * wigner_3j
+
+
+def calc_prefactor_of_operator_in_coupled_scheme(
+    f1: float, f2: float, f12: float, i1: float, i2: float, i12: float, kappa: int
+) -> float:
+    r"""Calculate the prefactor of the reduced matrix element for an operator acting on a state in a coupled scheme.
+
+    Here we follow equation (7.1.7) from Edmonds 1985 "Angular Momentum in Quantum Mechanics".
+    This means, for f2 = i2 (i.e. the operator only acts on the first quantum number),
+    the reduced matrix element is given by
+
+    .. math::
+        \langle f1, f2, f12 || \hat{O}_{\kappa} || i1, i2, i12 \rangle
+        = (-1)^{f1 + i2 + i12 + \kappa} * sqrt((2 * f12 + 1)(2 * i12 + 1)) * \text{Wigner6j}(f1, f12, i2; i12, i1, \kappa)
+            * \langle f1 || \hat{O}_{\kappa} || i1 \rangle
+        = prefactor  * \langle f1 || \hat{O}_{\kappa} || i1 \rangle
+
+    This function calculates and returns the prefactor.
+
+    Args:
+        f1: The quantum number of the first particle of the final state.
+        f2: The quantum number of the second particle of the final state.
+        f12: The total quantum number of the final state.
+        i1: The quantum number of the first particle of the initial state.
+        i2: The quantum number of the second particle of the initial state.
+        i12: The total quantum number of the initial state.
+        kappa: The rank :math:`\kappa` of the operator.
+
+    """
+    if f2 != i2:
+        raise ValueError("calc_prefactor_of_operator_in_coupled_scheme is meant to be used for f2 == i2 only.")
+    return (  # type: ignore [no-any-return]
+        minus_one_pow(f1 + i2 + i12 + kappa)
+        * np.sqrt((2 * f12 + 1) * (2 * i12 + 1))
+        * calc_wigner_6j(f1, f12, i2, i12, i1, kappa)
+    )
